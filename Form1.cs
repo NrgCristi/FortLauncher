8e9d620d79ac1a3c35897d0dfb22c1368d05913d
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-10-08 16:22:35 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FortLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LaunchBtn_Click(object sender, EventArgs e)
+        {
+            string FNPath = PathTextBox.Text;
+            string FNExePath = System.IO.Path.Combine(this.PathTextBox.Text, "FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe");
+            string FNExeName = "FortniteClient-Win64-Shipping.exe";
+            string LaunchArgs = "-AUTH_LOGIN=" + MailTextBox.Text + " -AUTH_PASSWORD=" + PassTextBox.Text + " -AUTH_TYPE=epic" + "-noeac -fromfl=be -fltoken=h1h4370717422124b232eFac -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal";
+            System.Diagnostics.Process.Start(FNExePath, LaunchArgs);
+        }
+    }
+}
